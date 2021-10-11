75eadfaf3fb8285a02934877ae4d7c32a5a0219f
fengchong <fengchong@126.com>
2021-10-11 09:20:55 +0800

    增加上次提交少提交的文件夹及文件

---
+++ controllers/aiccbreadcrumbnavigation.hpp
@@ -0,0 +1,22 @@
+#ifndef AICCBREADCRUMBNAVIGATION_H
+#define AICCBREADCRUMBNAVIGATION_H
+
+#include <QLabel>
+#include <QDebug>
+
+class AICCBreadcrumbNavigation : public QLabel
+{
+    Q_OBJECT
+public:
+    AICCBreadcrumbNavigation(QWidget *parent=nullptr){
+//        QString str_navigation = QString("<h1><i>Hello</i> <font color = red>Qt!</font></h1><h2>My CSDN blog: <a href=\"https://blog.csdn.net/humanking7\">https://blog.csdn.net/humanking7 </a></h2>");
+//        this->setText(str_navigation);
+//        this->set
+//        qDebug() << str_navigation << endl;
+    };
+
+public:
+//    void set
+};
+
+#endif // AICCBREADCRUMBNAVIGATION_H
---
+++ controllers/aicctoolbutton.hpp
@@ -0,0 +1,64 @@
+#ifndef AICCTOOLBUTTON_H
+#define AICCTOOLBUTTON_H
+
+#include <QToolButton>
+#include <QMouseEvent>
+#include <QDebug>
+#include <QDrag>
+#include <QMimeData>
+
+class AICCToolButton : public QToolButton
+{
+    Q_OBJECT
+public:
+    AICCToolButton(QWidget *parent=nullptr):QToolButton(parent){};
+    void setNodeName(QString name){_nodeName = name;}
+    void setNodeCaption(QString caption){_nodeCaption = caption;}
+protected:
+    void mousePressEvent(QMouseEvent *e)
+    {
+        if(e->button() && Qt::LeftButton){
+            QByteArray dataItem;
+            QDataStream dataStream(&dataItem,QIODevice::WriteOnly);
+            dataStream << _nodeName;
+
+            QByteArray dataItemName;
+            QDataStream dataStreamName(&dataItemName,QIODevice::WriteOnly);
+            QString sidata = _nodeCaption;
+            dataStreamName << sidata;
+
+            QDrag *drag = new QDrag(this);
+            QMimeData *mimeData = new QMimeData();
+            mimeData->setData("Data/text",dataItem);
+            mimeData->setData("Data/name",dataItemName);
+
+            drag->setMimeData(mimeData);
+            drag->exec(Qt::MoveAction);
+        }
+
+//        qDebug() << "mouse press event" << dataItem << " "<< dataItemName;
+        e->accept();
+    }
+    void mouseReleaseEvent(QMouseEvent *e)
+    {
+        qDebug() << "mouse release event";
+        e->accept();
+    };
+    void mouseMoveEvent(QMouseEvent *e)
+    {
+        qDebug() << "mouse move event";
+        e->accept();
+    };
+
+signals:
+    void preperNewTestButton();
+private slots:
+
+private:
+    QPoint _pressPoint;
+    QString _nodeName;
+    QString _nodeCaption;
+
+};
+
+#endif // AICCTOOLBUTTON_H
